a55d30bd0aab231022abf6e95e16dff0e3700ae1
jayven <kassarar@gmail.com>
2019-02-13 20:53:30 +0800

    Remove RawMsgXXX interfaces.

--- nproto/msg.go
+++ nproto/msg.go
@@ -22,3 +22,47 @@ type MsgSubscriber interface {
 
 // MsgHandler handles the message. The message should be redelivered if it returns an error.
 type MsgHandler func(context.Context, proto.Message) error
+
+// MsgAsyncPublisher extends MsgPublisher with PublishAsync for higher performance.
+type MsgAsyncPublisher interface {
+	// It's trivial to implement Publish if it supports PublishAsync. See MsgAsyncPublisherFunc.
+	MsgPublisher
+
+	// PublishAsync publishes a message to a given subject asynchronously.
+	// NOTE: If PublishAsync returns nil, then the final result must be returned by calling cb
+	// exactly once even context is done. Otherwise, cb must not called.
+	PublishAsync(ctx context.Context, subject string, msg proto.Message, cb func(error)) error
+}
+
+// MsgAsyncPublisherFunc is an adapter to allow the use of ordinary functions as MsgAsyncPublisher.
+type MsgAsyncPublisherFunc func(context.Context, string, proto.Message, func(error)) error
+
+var (
+	_ MsgAsyncPublisher = (MsgAsyncPublisherFunc)(nil)
+)
+
+// Publish implements MsgAsyncPublisher interface.
+func (fn MsgAsyncPublisherFunc) Publish(ctx context.Context, subject string, msg proto.Message) error {
+	var (
+		err  error
+		errc = make(chan struct{})
+	)
+	if err1 := fn(ctx, subject, msg, func(err2 error) {
+		err = err2
+		close(errc)
+	}); err1 != nil {
+		return err1
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-errc:
+		return err
+	}
+}
+
+// PublishAsync implements MsgAsyncPublisher interface.
+func (fn MsgAsyncPublisherFunc) PublishAsync(ctx context.Context, subject string, msg proto.Message, cb func(error)) error {
+	return fn(ctx, subject, msg, cb)
+}
